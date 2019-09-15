9fc25cf7e97fd639f41bf9f9282333cb1549de52
liu2010 <liu2010@qq.com>
2019-09-15 15:22:22 +0800

    第一次提交---init 添加test

--- src/main/java/H.java
+++ src/main/java/H.java
@@ -1,5 +1,10 @@
 public class H {
     public static void main(String[] args) {
         System.out.println(1);
+        test1();
+    }
+
+    public static void test1(){
+        System.out.println(2);
     }
 }
